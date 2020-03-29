50ea4959cac400153fccd402ecd53c81e6fdead1
DDavide-hub <davoliodavide@gmail.com>
2020-03-29 11:09:54 +0200

    change program

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const button = document.getElementById("incrementa-btn")
-const counter = document.getElementById("contatore-lbl")
 
-let counterValue = 0
-const increment = () => {
-  counterValue ++
-  counter.innerHTML = counterValue
-}
-
-button.onclick = increment
