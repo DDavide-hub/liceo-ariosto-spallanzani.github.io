c6dd460650ab8a27d67d04a14e1e65aa024658a4
DDavide-hub <davoliodavide@gmail.com>
2020-03-29 11:11:50 +0200

    aggiunti bottoni numeri e comando x inserirli

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+document.getElementById("button1").onclick = click1();
+document.getElementById("button2").onclick = click2();
+document.getElementById("button3").onclick = click3();
+document.getElementById("button4").onclick = click4();
+document.getElementById("button5").onclick = click5();
+document.getElementById("button6").onclick = click6();
+document.getElementById("button7").onclick = click7();
+document.getElementById("button8").onclick = click8();
+document.getElementById("button9").onclick = click9();
+document.getElementById("button0").onclick = click0();
+const operation = document.getElementById("operation")
+
+
+function click1() {
+  if (operation==="0") ? operation.innerHTML = "1" : operation.innerHTML += "1"; 
+}
+function click2() {
+  if (operation==="0") ? operation.innerHTML = "2" : operation.innerHTML += "2"; 
+}
+function click3() {
+  if (operation==="0") ? operation.innerHTML = "3" : operation.innerHTML += "3"; 
+}
+function click4() {
+  if (operation==="0") ? operation.innerHTML = "4" : operation.innerHTML += "4"; 
+}
+function click5() {
+  if (operation==="0") ? operation.innerHTML = "5" : operation.innerHTML += "5"; 
+}
+function click6() {
+  if (operation==="0") ? operation.innerHTML = "6" : operation.innerHTML += "6"; 
+}
+function click7() {
+  if (operation==="0") ? operation.innerHTML = "7" : operation.innerHTML += "7"; 
+}
+function click8() {
+  if (operation==="0") ? operation.innerHTML = "8" : operation.innerHTML += "8"; 
+}
+function click9() {
+  if (operation==="0") ? operation.innerHTML = "9" : operation.innerHTML += "9"; 
+}
+function click0() {
+  if (operation==="0") ? operation.innerHTML = "0" : operation.innerHTML += "0"; 
+}
 
